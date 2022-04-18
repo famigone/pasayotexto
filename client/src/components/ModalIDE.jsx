3643099102f919044972af5659e3761c9ba7b74c
fede.amigone@gmail.com <fede.amigone@gmail.com>
2022-04-18 20:58:26 -0300

    asdf

---
+++ client/src/components/ModalIDE.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Button, Modal } from 'react-bootstrap';
+import FormExperiencia from './FormExperiencia'
+import LogoPasayo from './LogoPasayo'
+
+
+
+const ModalIDE = (props) => {
+
+
+  return (
+    <>
+
+
+      <Modal
+        {...props}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+              <LogoPasayo/>
+               Nueva Experiencia
+
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormExperiencia {...props}/>
+        </Modal.Body>
+
+      </Modal>
+    </>
+  );
+}
+
+
+export default ModalIDE;
