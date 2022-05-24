4049a4f42827d419dcbf5ce9d65789eebbe4957b
Fede <fede.amigone@gmail.com>
2022-05-24 14:50:26 -0300

    asdf

--- client/src/components/CodeMirror2.jsx
+++ client/src/components/CodeMirror2.jsx
@@ -21,8 +21,8 @@ let socket;
 
 const CodeMirror2 = ({...props}) => {
   const [codigo, setCodigo] = useState('');
-  //const link= "http://localhost:8000/canal/"+props.experiencia+"/"+props.canal
-  const link= "https://pasayotexto.fi.uncoma.edu.ar/canal/"+props.experiencia+"/"+props.canal
+  const link= "http://localhost:8000/canal/"+props.experiencia+"/"+props.canal
+  //const link= "https://pasayotexto.fi.uncoma.edu.ar/canal/"+props.experiencia+"/"+props.canal
 
   const options = {
     lineNumbers: true,
