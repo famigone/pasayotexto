a2c975aabe1e5ad9689a954a3f2c3ffb7c3c5e33
Fede <fede.amigone@gmail.com>
2022-06-07 16:04:30 -0300

    asdf

--- server.js
+++ server.js
@@ -28,11 +28,12 @@ app.use( (req, res, next) => {
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-//app.use(cors())
-app.use(cors({
-  origin: "https://pasayotexto.fi.uncoma.edu.ar" ,
-  credentials: true
-}))
+app.use(cors())
+//app.use(cors({
+//  origin: "https://pasayotexto.fi.uncoma.edu.ar" ,
+//  credentials: true
+//}))
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
