046786a6c1ad7640cf2b9f068de488c4b031ff11
fede.amigone@gmail.com <fede.amigone@gmail.com>
2022-08-17 21:54:48 -0300

    asd

--- client/src/customHooks/UseInfinite.js
+++ client/src/customHooks/UseInfinite.js
@@ -1,18 +1,24 @@
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
+import api from '../api'
 
-function useInfinite(query, page) {
+function UseInfinite(query, page) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [list, setList] = useState([]);
 
   const sendQuery = useCallback(async () => {
     try {
-      await setLoading(true);
-      await setError(false);
-      const res = await axios.get(url);
-      await setList((prev) => [...prev, ...res.data];
-      setLoading(false);
+      setLoading(true);
+      setError(false);
+      query.limite= page
+      const res = await api.getAllExperiencias(query)      
+      setList((prev) => [...prev, ...res.data.data]);
+      console.log("query "+ query.tema)
+      console.log("page "+ page)
+      console.log("lista "+ list.length)
+      console.log("lista "+ list)
+      setLoading(false)
     } catch (err) {
       setError(err);
     }
@@ -25,4 +31,4 @@ function useInfinite(query, page) {
   return { loading, error, list };
 }
 
-export default useInfinite;
+export default UseInfinite;
--- client/src/pages/Comunidad1.jsx
+++ client/src/pages/Comunidad1.jsx
@@ -4,7 +4,7 @@ import ModalIDE from "../components/ModalIDE"
 import Card from '../components/Card'
 import Filtro from '../components/Filtro'
 import styled from 'styled-components'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { get } from 'axios';
 import api from '../api'
 import { createContext, useContext, useMemo } from "react";
@@ -12,105 +12,73 @@ import  UserProvider  from '../components/UserProvider';
 import  UserContext  from '../components/UserContext';
 import AuthService from "../services/auth.service";
 import UnSpinnerCentrado from "../components/UnSpinnerCentrado"
+import UseInfinite from "../customHooks/UseInfinite";
 import {
   useNavigate,
   useLocation,
   Navigate,
 } from "react-router-dom";
 
-const Comunidad = () => {
-  //const filtroInicial = {tema: "Todos", mias:true}
-  //const  { user }  = useContext(UserContext);
+const Comunidad1 = () => {
   
-  const constIncremento = 10
-  const constLimite = 10
+  
+  const constIncremento = 3
+  const constLimite = 2
   const filtroInicial = {tema: "TODOS"}
   const [experiencias, setExperiencias] = useState([])
   const [filtro, setFiltro] = useState(filtroInicial)
   const [limite, setLimite] = useState(constLimite)
   const [canal, setCanal] = useState("")
-  const [loading, setLoading] = useState(false)
+  
   const [user, setUser] = useState(AuthService.getCurrentUser().username)
   //expActual es la experiencia clickeada actualmente, al clickear debe levantar
   //el modalIDEShow con los datos de esa exp
   const [expActual, setExpActual] = useState("")
   const [modalIDEShow, setModalIDEShow] = useState(false);
+  const { loading, error, list } = UseInfinite(filtro, limite);
+  
+  const loader = useRef(null);
+
   let navigate = useNavigate();
   let location = useLocation();
   let from = location.state?.from?.pathname || "/login";
 
-  const Divido = styled.nav.attrs({
-      className: 'div',
-  })`
-
-  height: 545px;
-  overflow-y: auto;
-  `
-
-  const onDelete = () => {   
-    getExperiencias({})
-  }
-
-  const getExperiencias = async(filtro) => {
-    try {
-      setLoading(true)
-      filtro.limite= limite
-      const response = await api.getAllExperiencias(filtro)
-      setExperiencias(response.data.data);
-      setLoading(false)
-      //si response falla por 401 o 403 => redirigir al login
-      console.log("response  ",response.status)
-
-    } catch(error) {
-      setLoading(false)
-      console.log('error', error);
-      //if (response.status == 401 || response.res.status == 403)
-        //console.log("a loguearse caraju  ",response.status)
-       // navigate(from, { replace: true });
-    }
-  }
-
-  useEffect(async() => {     
-     await getExperiencias(filtro);     
-     //console.log("limite ",limite)
-   }, [limite]);
 
 
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setLimite((prev) => prev + constIncremento);
+      console.log("llamo al pinche handleObserver")
+    }    
+  }, []);
 
 
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 0
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observer.observe(loader.current);
+  }, [handleObserver]);
   
 
   const handleFiltro = (newFiltro) => {
       console.log(newFiltro);
       setFiltro(newFiltro)
-      getExperiencias(newFiltro);
+     
     }
 
   const handleClickExp = (exp, canal) => {
       setExpActual(exp)
       setModalIDEShow(true)
       setCanal(canal)
-    //  console.log(exp)
-      //getExperiencias(newFiltro);
-      //console.log(newFiltro);
+
     }
 
-
-
-
-   const handleScroll = (e) => {
-       const { offsetHeight, scrollTop, scrollHeight} = e.target
-
-       if (offsetHeight + scrollTop >= scrollHeight) {
-         setLimite(limite+constIncremento)
-         getExperiencias(filtro)
-
-        // console.log(limite+constIncremento)
-       }
-       //console.log(limite+constIncremento)
-       //setLimite(limite+constIncremento)
-
-     }
+ 
 
      const handleCargar = () => {
            setLimite(limite+constIncremento)
@@ -121,20 +89,23 @@ const Comunidad = () => {
       return <UnSpinnerCentrado/>
      }
 
-  console.log("U S E R "+user)   
+  const onDelete = () => {}
   return (  
-  <div>
-    <Filtro user={user}
-      refrescarExp={getExperiencias}
+    
+  <>
+    
+    <Filtro user={user}      
       handleFiltro={handleFiltro}
       handleCargar={handleCargar}/>
-    <StackGrid columnWidth={300}>
-      {(!loading) && experiencias.map((exp) => {
+    
+      {(!loading) && list.map((exp) => {
               return(
-                <Card onDelete={onDelete} key={exp._id} experiencia={exp} handleClickExp={handleClickExp} canal={Math.random().toString(36).slice(2)}/>
-              )
-            })}      
-    </StackGrid>
+                <Card onDelete={onDelete} key={exp._id} experiencia={exp} handleClickExp={handleClickExp} canal={Math.random().toString(36).slice(2)}/>                
+              )              
+            })}                     
+    
+    <div>{loading && "Cargando experiencias..."}</div> 
+    <div ref={loader}/>
     {loading && lanzarSpinner()}      
     {!user && lanzarSpinner()}      
     <ModalIDE experiencia={expActual}
@@ -143,9 +114,9 @@ const Comunidad = () => {
               canal={canal}
               user={user}
               />
-</div>
+</>
 
   )
 }
 
-export default Comunidad;
+export default Comunidad1;
